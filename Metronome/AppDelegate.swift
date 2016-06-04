92d4f0d47104cebb3cfc99b82f124fb1e0637c56
Pawel Scibek <pawel.scibek@polidea.com>
2016-06-04 22:23:53 +0200

    Initial commit

---
+++ Metronome/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Metronome
+//
+//  Created by Pawel Scibek on 04/06/16.
+//  Copyright © 2016 Pawel Scibek. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        
+        
+        return true
+    }
+
+}
+
